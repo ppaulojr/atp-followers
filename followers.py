8539f383c8672c827f63f2f7536d4cde4c4ad7b5
Pedro Paulo Jr <ppaulojr@gmail.com>
2015-01-07 21:59:30 -0200

    TODO: Update ATP Top15

--- followers.py
+++ followers.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
 from twython import Twython
 import datetime
 print datetime.datetime.now()
+
+# TODO: Update twitter handle of the players
+
 players = ["fabiofogna",
           "richardgasquet1",
           "JohnIsner",
@@ -15,9 +20,11 @@ players = ["fabiofogna",
           "RafaelNadal",
           "rogerfederer",
           "DjokerNole"]
+          
 APIKEY = "REGISTER_AN_API_KEY"
 APISEC = "GRAB_YOUR_API_SECRET"
 twitter = Twython(APIKEY,APISEC)
+
 for p in players:
      followers = twitter.show_user(screen_name = p)
      print (p,followers['followers_count'])
